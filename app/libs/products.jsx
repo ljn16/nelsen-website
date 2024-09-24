e38246f11be216d099beea7bb2100a504c79ec44
Logan Nelsen <loganjnelsen@gmail.com>
2024-09-24 15:05:37 -0500

    w product imgs

--- app/libs/products.jsx
+++ app/libs/products.jsx
@@ -1,8 +1,8 @@
 import '../styles/section.scss'
-// import Image from 'next/image'
-// import reactime from '../assets/products/reactime.gif'
-// import cogntor from '../assets/products/Cogntor.png'
-// import workbrew from '../assets/products/Workbrew.png'
+import Image from 'next/image'
+import reactime from '../assets/products/Reactime.gif'
+import cogntor from '../assets/products/Cogntor.png'
+import workbrew from '../assets/products/Workbrew.png'
 
 function Products() {
 
@@ -20,7 +20,7 @@ function Products() {
               <h4>Reactime</h4>
               <span className='product-description'>React Metrics Monitoring Application</span>
             </span>
-            {/* <Image src={reactime} alt='Reactime product image' width={500} height={500} /> */}
+            <Image src={reactime} alt='Reactime product image' width={500} height={500} />
           </span>
 
           {/* <span className='product'>
@@ -36,7 +36,7 @@ function Products() {
               <h4>Cogntor</h4>
               <span className='product-description'>Attention Metric Monitoring Application</span>
             </span>
-            {/* <Image src={cogntor} alt='Cogntor product image' width={500} height={500} /> */}
+            <Image src={cogntor} alt='Cogntor product image' width={500} height={500} />
           </span>
           
           
@@ -45,7 +45,7 @@ function Products() {
               <h4>Work Brew</h4>
               <span className='product-description'>Café Locator Application</span>
             </span>
-            {/* <Image src={workbrew} alt='Work Brew product image' width={500} height={500} /> */}
+            <Image src={workbrew} alt='Work Brew product image' width={500} height={500} />
           </span>
         </div>
       {/* </div> */}
