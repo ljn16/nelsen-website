edaa0a11a17d0765fb40e33dcbb1aa46281899d2
Logan Nelsen <loganjnelsen@gmail.com>
2024-09-24 14:18:51 -0500

    no imgs

--- app/libs/products.jsx
+++ app/libs/products.jsx
@@ -1,8 +1,8 @@
 import '../styles/section.scss'
 import Image from 'next/image'
-import reactime from '../assets/products/reactime.gif'
-import cogntor from '../assets/products/Cogntor.png'
-import workbrew from '../assets/products/Workbrew.png'
+// import reactime from '../assets/products/reactime.gif'
+// import cogntor from '../assets/products/Cogntor.png'
+// import workbrew from '../assets/products/Workbrew.png'
 
 function Products() {
 
@@ -36,7 +36,7 @@ function Products() {
               <h4>Cogntor</h4>
               <span className='product-description'>Attention Metric Monitoring Application</span>
             </span>
-            <Image src={cogntor} alt='Cogntor product image' width={500} height={500} />
+            {/* <Image src={cogntor} alt='Cogntor product image' width={500} height={500} /> */}
           </span>
           
           
@@ -45,7 +45,7 @@ function Products() {
               <h4>Work Brew</h4>
               <span className='product-description'>Café Locator Application</span>
             </span>
-            <Image src={workbrew} alt='Work Brew product image' width={500} height={500} />
+            {/* <Image src={workbrew} alt='Work Brew product image' width={500} height={500} /> */}
           </span>
         </div>
       {/* </div> */}
--- app/libs/salutation.jsx
+++ app/libs/salutation.jsx
@@ -1,19 +1,19 @@
 import '../styles/salutation.scss'
 
 import Image from 'next/image'
-import html from '../assets/logos/html5-logo-transparent.png'
-import css from '../assets/logos/css3-logo-transparent.png'
-import react from '../assets/logos/react-logo-transparent.png'
-import sass from '../assets/logos/sass-logo.png'
-import ts from '../assets/logos/typescript-logo.png'
-import redux from '../assets/logos/redux-logo.png'
-import express from '../assets/logos/express-logo.png'
-import node from '../assets/logos/nodejs-logo.png'
-import mongo from '../assets/logos/mongodb-logo.svg'
-import python from '../assets/logos/python-logo.png'
-import git from '../assets/logos/git-logo.png'
-import svelte from '../assets/logos/svelte-logo.png'
-import next from '../assets/logos/nextjs-icon.png'
+// import html from '../assets/logos/html5-logo-transparent.png'
+// import css from '../assets/logos/css3-logo-transparent.png'
+// import react from '../assets/logos/react-logo-transparent.png'
+// import sass from '../assets/logos/sass-logo.png'
+// import ts from '../assets/logos/typescript-logo.png'
+// import redux from '../assets/logos/redux-logo.png'
+// import express from '../assets/logos/express-logo.png'
+// import node from '../assets/logos/nodejs-logo.png'
+// import mongo from '../assets/logos/mongodb-logo.svg'
+// import python from '../assets/logos/python-logo.png'
+// import git from '../assets/logos/git-logo.png'
+// import svelte from '../assets/logos/svelte-logo.png'
+// import next from '../assets/logos/nextjs-icon.png'
 
 
 function Salutation() {
@@ -23,7 +23,7 @@ function Salutation() {
 
         <div className='logo-banner-wrapper'>
             <div className='logo-banner'>
-                <Image src={html} alt="HTML5 Logo"  width={500} height={500}/>
+                {/* <Image src={html} alt="HTML5 Logo"  width={500} height={500}/>
                 <Image src={css} alt="CSS3 Logo"  width={500} height={500}/>
                 <Image src={react} alt="React Logo"  width={500} height={500}/>
                 <Image src={sass} alt="Sass Logo"  width={500} height={500}/>
@@ -35,10 +35,10 @@ function Salutation() {
                 <Image src={python} alt="Python Logo"  width={500} height={500}/>
                 <Image src={git} alt="Git Logo"  width={500} height={500}/>
                 <Image src={svelte} alt="Svelte Logo"  width={500} height={500}/>
-                <Image src={next} alt="Next.js Logo"  width={500} height={500}/>
+                <Image src={next} alt="Next.js Logo"  width={500} height={500}/> */}
             </div>
             <div className='logo-banner'>
-                <Image src={html} alt="HTML5 Logo"  width={500} height={500}/>
+                {/* <Image src={html} alt="HTML5 Logo"  width={500} height={500}/>
                 <Image src={css} alt="CSS3 Logo"  width={500} height={500}/>
                 <Image src={react} alt="React Logo"  width={500} height={500}/>
                 <Image src={sass} alt="Sass Logo"  width={500} height={500}/>
@@ -50,7 +50,7 @@ function Salutation() {
                 <Image src={python} alt="Python Logo"  width={500} height={500}/>
                 <Image src={git} alt="Git Logo"  width={500} height={500}/>
                 <Image src={svelte} alt="Svelte Logo"  width={500} height={500}/>
-                <Image src={next} alt="Next.js Logo"  width={500} height={500}/>
+                <Image src={next} alt="Next.js Logo"  width={500} height={500}/> */}
 
             </div>
         </div>
