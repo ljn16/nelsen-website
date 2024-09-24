be10225c8cb3b365e475a822b7f2fa1d19a78556
Logan Nelsen <loganjnelsen@gmail.com>
2024-09-24 15:01:52 -0500

    ..

--- app/libs/nav.jsx
+++ app/libs/nav.jsx
@@ -1,8 +1,8 @@
 import '../styles/nav.scss'
 // import SideNav from './sideNav';
 import Image from 'next/image';
-// import githubLogo from '/app/assets/logos/github-logo.png';
-import linkedInLogo from '/app/assets/logos/linkedIn-logo.png';
+import githubLogo from '/app/assets/logos/github-logo.png';
+import linkedInLogo from '/app/assets/logos/linkedin-logo.png';
 
 
 function Nav() {
@@ -18,10 +18,10 @@ function Nav() {
           </div>
           <div className='profile-link-icons'>
             <a href='https://github.com/ljn16' target='_blank'>
-                {/* <Image src={githubLogo} alt='GitHub Logo' width={96} height={96} /> */}
+                <Image src={githubLogo} alt='GitHub Logo' width={96} height={96} />
             </a>
             <a href='https://www.linkedin.com/in/logan-nelsen/' target='_blank'>
-                <Image src={linkedInLogo} alt='LinkedIn Logo' width={96} height={96} />
+                <Image src={linkedinLogo} alt='LinkedIn Logo' width={96} height={96} />
             </a>
           </div>
         </div>
