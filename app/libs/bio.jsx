6ed77d020c70644207380467fa10cb6cfaebbee2
Logan Nelsen <loganjnelsen@gmail.com>
2024-09-28 17:15:55 -0500

    nav rewritten with tailwind

--- app/libs/bio.jsx
+++ app/libs/bio.jsx
@@ -12,7 +12,7 @@ function Biography() {
         <h2>About Me</h2>
         <p>I am a software engineer with a passion for building innovative, intuitive full-stack applications to solve real-world problems with user psychology/cognition in mind. A broad range of experience using React, SCSS, Node.js, SQL/NoSQL, and more.</p>
 
-        <Salutation/>
+        {/* <Salutation/> */}
       </div>
     </>
   )
--- app/libs/nav.jsx
+++ app/libs/nav.jsx
@@ -1,4 +1,4 @@
-import '../styles/nav.scss'
+// import '../styles/nav.scss'
 // import SideNav from './sideNav';
 import Image from 'next/image';
 // import githubLogo from '/app/assets/logos/github-logo.png';
@@ -11,37 +11,72 @@ import instagramLogo from '/app/assets/logos/instagram-logo.png';
 function Nav() {
   return (
     <>
-      <nav>
-        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-          <p style={{ backgroundColor: 'rgba(255, 255, 0, 0.5)', color: 'black', borderRadius: 5, padding: 5, paddingLeft: 10, paddingRight: 10, margin: 10 }}>&zwnj;🚧 Site under construction 🚧&zwnj;</p>
-        </div>
-        <div className='nav-content'>
-          {/* <a href='#home'>X</a>
-          <a href='#about'>Y</a>
-          <a href='#projects'>Z</a> */}
-          <div>
-            {/* <SideNav/> */}
-          </div>
-          <div className='profile-link-icons'>
-            <a href='https://github.com/ljn16' target='_blank'>
-                <div style={{ width: '4vh', height: '4vh', position: 'relative', marginLeft: '20px'}}>
-                  <Image src={githubLogo2} alt='GitHub Logo' layout='fill' objectFit='contain' />
-                </div>
-            </a>
-            <a href='https://www.linkedin.com/in/logan-nelsen/' target='_blank'>
-                <div style={{ width: '4vh', height: '4vh', position: 'relative', marginLeft: '10px'}}>
-                  <Image src={linkedinLogo} alt='LinkedIn Logo' layout='fill' objectFit='contain' />
-                </div>
-            </a>
+      <div className='flex justify-center items-center fixed top-0 left-0 w-full z-50'>
+        <div className='
+        flex justify-evenly align-center
+        
+        bg-glass backdrop-blur-lg
+        h-[5vh] w-full
+        rounded-lg p-1 m-2
+        '>
+          <nav className='grid grid-cols-3 gap-4  h-full w-full
+          rounded-b-lg'>
+            {/* //!LEFT */}
+            <div className='
+            flex
+            rounded-bl-lg
+            h-full'> 
 
-            <a href='https://www.instagram.com/logan_nelsen/' target='_blank'>
-                <div style={{ width: '4vh', height: '4vh', position: 'relative', marginLeft: '10px', marginRight: '20px' }}>
-                  <Image src={instagramLogo} alt='LinkedIn Logo' layout='fill' objectFit='contain' />
-                </div>
-            </a>
-          </div>
+
+
+            </div>
+            {/* //!CENTER */}
+            <div className='flex justify-center items-center
+            h-full'> 
+              <span className='
+              bg-yellow-400 p-1 rounded-md opacity-50 text-black
+              '> {/* style={{ backgroundColor: 'rgba(255, 255, 0, 0.5)', color: 'black', borderRadius: 5, padding: 5, paddingLeft: 10, paddingRight: 10, margin: 10 }} */}
+                
+                &zwnj;🚧 Site under construction 🚧&zwnj;
+              </span>
+
+
+
+            </div>
+            {/* //!RIGHT */}
+            <div className='
+            flex justify-end 
+            h-full w-full
+            rounded-br-lg'> 
+              <div className='
+              flex justify-around items-center
+              bg-glassOver h-full w-1/4
+              rounded-lg
+              ' >
+                <a href='https://github.com/ljn16' target='_blank' className='
+                  relative h-3/4 aspect-square
+                  bg-yellow-600 rounded-md'>
+                  <div ></div>
+                </a>
+                <a href='https://www.linkedin.com/in/logan-nelsen/' target='_blank' className='
+                  relative h-3/4 aspect-square
+                  bg-yellow-600 rounded-md'>
+                  <div ></div>
+                </a>
+                <a href='https://www.instagram.com/logan_nelsen/' target='_blank' className='
+                  relative h-3/4 aspect-square
+                  bg-yellow-600 rounded-md'>
+                  <div ></div>
+                </a>
+                
+              </div>
+
+
+
+            </div>
+          </nav>
         </div>
-      </nav>
+      </div>
     </>
   )
 }
