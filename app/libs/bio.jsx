10381a32434c157dd75b7bcce00f417073a8dd48
Logan Nelsen <loganjnelsen@gmail.com>
2024-10-24 16:35:23 -0500

    ..

--- app/libs/bio.jsx
+++ app/libs/bio.jsx
@@ -19,7 +19,7 @@ function Biography() {
         top-80 left-0 w-full md:left-3/4 md:top-10
         bg-white bg-opacity-50 md:bg-opacity-15 rounded-lg p-4 
         md:min-w-[300px]'>
-          <h2 className='text-green-500'>hi</h2>
+          <h2 className='text-green-500'>Hi, I&apos;m Logan</h2>
           <p className=''>I am a software engineer with a passion for building innovative, intuitive full-stack applications to solve real-world problems with user psychology/cognition in mind. A broad range of experience using React, Sass, Node.js, SQL/NoSQL, and more.</p>
         </div>
       </div>
